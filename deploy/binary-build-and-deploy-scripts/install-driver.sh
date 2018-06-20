6a851bc96872b1895b740d18070bda7c6cbc5acd
Falk Pollok <falk.pollok@ibm.com>
2018-06-20 14:06:57 -0400

    Fixed path for s3fs to be /usr/local/bin rather than /local/bin.

--- deploy/binary-build-and-deploy-scripts/install-driver.sh
+++ deploy/binary-build-and-deploy-scripts/install-driver.sh
@@ -4,8 +4,7 @@ set -ex
 DRIVER_LOCATION="/host/usr/libexec/kubernetes/kubelet-plugins/volume/exec/ibm~ibmc-s3fs"
 KUBELET_SVC_CONFIG="/host/lib/systemd/system/kubelet.service"
 
-mkdir -p /host/local/bin
-cp /root/bin/s3fs /host/local/bin/
+cp /root/bin/s3fs /host/usr/local/bin/
 cp /root/bin/install-dep.sh /host/root/
 chmod +x /host/local/bin/s3fs /host/root/install-dep.sh 
 
