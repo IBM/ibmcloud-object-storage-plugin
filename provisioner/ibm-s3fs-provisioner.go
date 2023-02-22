19af0aad2db634c5195179a5c8a331d90bde3daf
bhagyashree <bhagyak1@in.ibm.com>
2023-02-22 19:09:44 +0530

    add gosec rule

--- provisioner/ibm-s3fs-provisioner.go
+++ provisioner/ibm-s3fs-provisioner.go
@@ -104,7 +104,7 @@ const (
 	defaultName          = "IBMGrpcProvider"
 	clusterTypeVpcG2     = "vpc-gen2"
 	clusterTypeClassic   = "cruiser"
-	ResConfApiKey        = "res-conf-apikey"
+	ResConfApiKey        = "res-conf-apikey"  // #nosec G101
 	KPRootKeyCRN         = "kp-root-key-crn"
 )
 
