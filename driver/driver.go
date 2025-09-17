ba0e9e9c7419296a2e0f5db5fc8162e23a40a7f2
bhagyak1 <bhagyak1@in.ibm.com>
2025-09-17 15:47:26 +0530

    migrate golangci-lint to v2

--- driver/driver.go
+++ driver/driver.go
@@ -2,7 +2,7 @@
  * IBM Confidential
  * OCO Source Materials
  * IBM Cloud Kubernetes Service, 5737-D43
- * (C) Copyright IBM Corp. 2017, 2023 All Rights Reserved.
+ * (C) Copyright IBM Corp. 2017, 2025 All Rights Reserved.
  * The source code for this program is not published or otherwise divested of
  * its trade secrets, irrespective of what has been deposited with
  * the U.S. Copyright Office.
