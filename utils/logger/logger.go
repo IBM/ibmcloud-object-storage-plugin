41404ed2d869fdf8447b6d93d5078f267bbc8878
Bhagyashreek8 <57988406+Bhagyashreek8@users.noreply.github.com>
2025-10-09 11:59:04 +0530

    fix gosec error checks and exception for gosec (#185)
    * fix gosec error checks and exception for gosec
    
    * fix gosec error checks and exception for gosec
    
    * fix gosec error checks and exception for gosec
    
    * fix gosec error checks and exception for gosec
    
    * fix gosec error checks and exception for gosec
    
    * fix gosec error checks and exception for gosec
    
    * fix gosec error checks and exception for gosec
    
    * fix gosec error checks and exception for gosec
    
    * fix gosec error checks and exception for gosec
    
    * fix gosec error checks and exception for gosec

--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -8,16 +8,6 @@
  * the U.S. Copyright Office.
  ******************************************************************************/
 
-/******************************************************************************r
- * IBM Confidential
- * OCO Source Materials
- * IBM Cloud Container Service, 5737-D43
- * (C) Copyright IBM Corp. 2017, 2018 All Rights Reserved.
- * The source code for this program is not  published or otherwise divested of
- * its trade secrets, irrespective of what has been deposited with
- * the U.S. Copyright Office.
- ******************************************************************************/
-
 package logger
 
 import (
